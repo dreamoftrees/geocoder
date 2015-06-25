00730aa88393ef81becf9471de65bf2dfe18bb86
Charlie McKenzie <charlie@dreamoftrees.com>
2015-06-25 15:20:12 +1000

    Mapbox geocoding support

---
+++ lib/geocoder/lookups/mapbox.rb
@@ -0,0 +1,60 @@
+require 'cgi'
+require 'geocoder/lookups/base'
+require "geocoder/results/google"
+
+module Geocoder::Lookup
+  class Mapbox < Base
+    def name
+      "Mapbox"
+    end
+
+    def required_api_key_parts
+      ["key"]
+    end
+
+    def supported_protocols
+      [:https]
+    end
+
+    def query_url(query)
+      #"#{protocol}://maps.googleapis.com/maps/api/place/details/json?#{url_query_string(query)}"
+      "#{protocol}://api.tiles.mapbox.com/v4/geocode/#{dataset}/#{URI.escape(query.sanitized_text.strip)}.json?#{url_query_string(query)}"
+    end
+
+    private
+
+    def results(query)
+      return [] unless doc = fetch_data(query)
+
+      # case doc["status"]
+      # when "OK"
+      #   return [doc["result"]]
+      # when "OVER_QUERY_LIMIT"
+      #   raise_error(Geocoder::OverQueryLimitError) || Geocoder.log(:warn, "Google Places Details API error: over query limit.")
+      # when "REQUEST_DENIED"
+      #   raise_error(Geocoder::RequestDenied) || Geocoder.log(:warn, "Google Places Details API error: request denied.")
+      # when "INVALID_REQUEST"
+      #   raise_error(Geocoder::InvalidRequest) || Geocoder.log(:warn, "Google Places Details API error: invalid request.")
+      # end
+
+      []
+    end
+
+    def query_url_params(query)
+      {
+          :api_key => configuration.api_key,
+          :q => query.sanitized_text
+      }.merge(super)
+    end
+
+    def dataset
+      configuration[:dataset] || "mapbox.places"
+    end
+
+    def mapbox_params
+      {
+          access_token: configuration.api_key
+      }
+    end
+  end
+end
